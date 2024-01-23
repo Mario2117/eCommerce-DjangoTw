280f76da0c85972392645a1d99c02d1442e085b3
Mario2117 <mariosgar2117@gmail.com>
2024-01-23 14:25:52 -0800

    Images display, signup and login page

--- eCom/urls.py
+++ eCom/urls.py
@@ -14,17 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 from cart.views import add_to_cart
-from core.views import frontpage,shop
+from core.views import frontpage,shop,signup,login
 from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('signup/', signup, name='signup'),
+    path('login/', login, name='login'),
     path('product/<slug:slug>/', product, name='product'),
     path('admin/', admin.site.urls),
     path('shop/', shop, name='shop'),
-    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
-]
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
