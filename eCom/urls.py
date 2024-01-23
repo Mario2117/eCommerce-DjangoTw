9b29f9d314760c83c9019197e2eabb12330d5393
Mario2117 <mariosgar2117@gmail.com>
2024-01-22 18:17:45 -0800

    Cart classes and functions. Add to cart.

--- eCom/urls.py
+++ eCom/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from cart.views import add_to_cart
 from core.views import frontpage,shop
 from product.views import product
 
@@ -25,4 +26,5 @@ urlpatterns = [
     path('product/<slug:slug>/', product, name='product'),
     path('admin/', admin.site.urls),
     path('shop/', shop, name='shop'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ]
