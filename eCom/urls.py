c261fd3278d9ae5e00f4c307cd7000f7217df401
Mario2117 <mariosgar2117@gmail.com>
2024-01-22 17:09:55 -0800

    Category search and price display.

--- eCom/urls.py
+++ eCom/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import frontpage
+from core.views import frontpage,shop
+from product.views import product
 
 urlpatterns = [
     path('', frontpage, name='frontpage'),
+    path('product/', product, name='product'),
     path('admin/', admin.site.urls),
+    path('shop/', shop, name='shop'),
 ]
