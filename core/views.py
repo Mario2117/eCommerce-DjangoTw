002ca13577ecadbd787e849ff3f30e0981c719ee
Mario2117 <mariosgar2117@gmail.com>
2024-01-29 20:37:04 -0800

    My account page and Edit account

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.db.models import Q
 
@@ -45,5 +46,20 @@ def signup(request):
 
     return render(request, 'core/signup.html', {'form':form})
 
-def login_old(request):
-    return render(request, 'core/login.html')
+@login_required
+def myaccount(request):
+    return render(request, 'core/myaccount.html')
+
+@login_required
+def edit_myaccount(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.save()
+
+        return redirect('myaccount')
+
+    return render(request, 'core/edit_myaccount.html')
